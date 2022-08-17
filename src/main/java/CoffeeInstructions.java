4f0801bb018f78b6f98abf8bf8a47352504ecc97
mycp98 <marcycalderon@hotmail.co.uk>
2022-08-17 16:44:49 +0100

    print out first four coffee instructions

---
+++ src/main/java/CoffeeInstructions.java
@@ -0,0 +1,15 @@
+public class CoffeeInstructions {
+    public static void main(String[] args) {
+        //Starting to make a coffee
+        //Grinding coffee beans
+        //Boiling water
+        //Mixing boiled water with crushed coffee beans
+        //Pouring coffee into the cup
+        //Pouring some milk into the cup
+        //Coffee is ready!
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+    }
+}
